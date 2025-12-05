eb44cf66a27af4bba3010bef40881c8ba69c0632
Aditya1234AD <158497215+Aditya1234AD@users.noreply.github.com>
2025-12-05 14:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.secret_key = os.getenv("SECRET_KEY", "local_dev_secret_key_1234567890")
 
 # ------------------- SUPABASE CONFIG -------------------
 SUPABASE_URL = os.getenv("SUPABASE_URL", "https://souedaocajeetpmdixme.supabase.co")
-SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY", "<put_service_role_key_in_env>")
+SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvdWVkYW9jYWplZXRwbWRpeG1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NTk2ODcsImV4cCI6MjA4MDQzNTY4N30.3QOeS3jpI6f1-auxKlYmUZCjZmJRqBomnINuy6xkn6Q")
 BUCKET_NAME = os.getenv("BUCKET_NAME", "uploads")
 
 # Create supabase client (use service role key server-side only)
